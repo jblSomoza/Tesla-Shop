6c4bd6ef528149abf7aa9b5fe22c8764d912cf88
Jhony Bryan López Somoza <Jhony.BLS@anacafe.org>
2022-10-12 08:16:16 -0600

    Added Carousel for images and also implemented counter

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
-import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import React, { FC, useMemo, useState } from 'react'
+import NextLink from 'next/link'
+import { Box, Card, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material'
 import { IProduct } from '../../interfaces'
 
 interface Props {
@@ -23,14 +24,19 @@ export const ProductCard:FC<Props> = ({ product }) => {
         onMouseLeave = {() => setIsHovered(false)}
     >
         <Card>
-          <CardActionArea>
-            <CardMedia    
-              component={'img'}
-              image={ productImage }
-              alt={ product.title }
-              className='fadeIn'              
-            />
-          </CardActionArea>
+          <NextLink href={`/product/slug`} passHref prefetch={false}>
+            <Link>
+              <CardActionArea>
+                <CardMedia    
+                  component={'img'}
+                  image={ productImage }
+                  alt={ product.title }
+                  className='fadeIn'              
+                />
+              </CardActionArea>
+            </Link>
+          </NextLink>
+          
         </Card>
 
         <Box sx={{ mt:1}} className='fadeIn'>
---
+++ components/products/ProductSlideshow.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import React, { FC } from 'react'
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+interface Props{
+    images: string[]
+}
+export const ProductSlideshow:FC<Props> = ({ images }) => {
+  return (
+    <Carousel
+      autoPlay
+      infiniteLoop
+      verticalSwipe='standard'
+    >
+      {
+        images.map((image) => {
+          const url = `/products/${image}`;
+          return(
+            <div key={url}>
+              <img src={url} />
+            </div>
+          )
+        })
+      }
+    </Carousel>
+  )
+}
