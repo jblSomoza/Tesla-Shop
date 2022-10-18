9a1846294cdd0dccb474135ef4d2451aa86a939d
Jhony Bryan López Somoza <Jhony.BLS@anacafe.org>
2022-10-18 15:45:27 -0600

    Added React Hooks for Data Fetching

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SWRConfig } from 'swr'
 
 import '../styles/globals.css'
 import { lightTheme } from '../themes';
@@ -12,10 +13,16 @@ import '@fontsource/roboto/700.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig 
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,24 @@ import type { NextPage } from 'next';
 
 import { ShopLayout } from '../components/layouts';
 import { ProductList } from '../components/products';
-import { initialData } from '../database/products';
+import { FullScreenLoading } from '../components/ui';
+import { useProducts } from '../hooks';
+
 
 const Home: NextPage = () => {
+
+  const { products, isLoading } = useProducts('/products');
+
   return (
     <ShopLayout title={'Teslo Shop - Home'} pageDescription={'Encuentra los mejores productos'}>
       <Typography variant='h1' component={'h1'}>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1}}>Todos los productos</Typography>
 
-      {/*  
-        ** ToDo: Especificamos as any solo para pasar los productos ya que la interfaz SeedProduct 
-        ** no tiene el atributo ._id por eso da conflicto de interfaces
-       */}
-      <ProductList 
-        products={initialData.products as any}
-      />
-
+      {
+        isLoading
+        ? <FullScreenLoading />
+        : <ProductList products={ products } />
+      }
       
     </ShopLayout>
   )
